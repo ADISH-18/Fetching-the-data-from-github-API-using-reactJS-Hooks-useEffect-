36df5023ad74693e2b4d5932b220ca63a074c9b9
ADISH-18 <adishharish1970@gamil.com>
2024-08-17 12:18:11 +0530

    upload file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// useEffect
+
+// function Example() {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//       const interval = setTimeout(() => { 
+//           setCount(count => count + 1); 
+//       }, 2000);
+
+//       return () => clearTimeout(interval); // Cleanup
+//   }, [count]);
+
+//   return (
+//       <div className="counter">
+//           <h1>Website counting detail {count} times</h1>
+//       </div>
+//   )
+// }
+
+function Demo() {
+  const [users, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      fetch('https://api.github.com/users')
+          .then(response => response.json())
+          .then(data => {
+              setUser(data);
+              setLoading(false);
+          })
+          .catch(error => {
+              console.error("Error loading data");
+              setLoading(false);
+          });
+  }, []);
+
+  if (loading) {
+      return <p className="loading">Loading...</p>;
+  }
+
+  return (
+      <div className="container">
+          <h1>List of GitHub Users</h1>
+          <ul className="user-list">
+              {users.map(user => (<li key={user.id}>{user.login}</li>))}
+          </ul>
+      </div>
+  )
+}
+
+function Final() {
+  return (
+      <div>
+          {/* <Example /> */}
+          <Demo />
+      </div>
+  )
+}
+
+ReactDOM.render(<Final />, document.getElementById('root'));
